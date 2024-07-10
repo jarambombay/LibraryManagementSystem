678c5365b4edf0f15f9c7a2b635397053442f78d
jarambombay <enjaramba@kabarak.ac.ke>
2024-07-10 18:19:06 +0300

    first commit

---
+++ LibraryManagementSystem/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnClear = New Button()
+        dgvBook = New DataGridView()
+        txtYearPublished = New TextBox()
+        txtGenre = New TextBox()
+        txtAuthor = New TextBox()
+        txtTitle = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        CType(dgvBook, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(36, 209)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(94, 29)
+        btnAdd.TabIndex = 0
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(36, 261)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(94, 29)
+        btnUpdate.TabIndex = 1
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(36, 315)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(94, 29)
+        btnDelete.TabIndex = 2
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(36, 366)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(94, 29)
+        btnClear.TabIndex = 3
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' dgvBook
+        ' 
+        dgvBook.AllowUserToAddRows = False
+        dgvBook.AllowUserToDeleteRows = False
+        dgvBook.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvBook.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3, Column4})
+        dgvBook.Location = New Point(352, 25)
+        dgvBook.Name = "dgvBook"
+        dgvBook.ReadOnly = True
+        dgvBook.RowHeadersWidth = 51
+        dgvBook.Size = New Size(514, 213)
+        dgvBook.TabIndex = 4
+        ' 
+        ' txtYearPublished
+        ' 
+        txtYearPublished.Location = New Point(164, 118)
+        txtYearPublished.Name = "txtYearPublished"
+        txtYearPublished.Size = New Size(125, 27)
+        txtYearPublished.TabIndex = 5
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(164, 166)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(125, 27)
+        txtGenre.TabIndex = 6
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(164, 72)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(125, 27)
+        txtAuthor.TabIndex = 7
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(164, 25)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(125, 27)
+        txtTitle.TabIndex = 8
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.Control
+        Label1.Location = New Point(56, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(38, 20)
+        Label1.TabIndex = 9
+        Label1.Text = "Title"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(56, 72)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 20)
+        Label2.TabIndex = 10
+        Label2.Text = "Author"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(56, 118)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(101, 20)
+        Label3.TabIndex = 11
+        Label3.Text = "YearPublished"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(56, 166)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(48, 20)
+        Label4.TabIndex = 12
+        Label4.Text = "Genre"
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Title"
+        Column1.MinimumWidth = 6
+        Column1.Name = "Column1"
+        Column1.ReadOnly = True
+        Column1.Width = 125
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Author"
+        Column2.MinimumWidth = 6
+        Column2.Name = "Column2"
+        Column2.ReadOnly = True
+        Column2.Width = 125
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "YearPublished"
+        Column3.MinimumWidth = 6
+        Column3.Name = "Column3"
+        Column3.ReadOnly = True
+        Column3.Width = 125
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Genre"
+        Column4.MinimumWidth = 6
+        Column4.Name = "Column4"
+        Column4.ReadOnly = True
+        Column4.Width = 125
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Menu
+        ClientSize = New Size(901, 450)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtTitle)
+        Controls.Add(txtAuthor)
+        Controls.Add(txtGenre)
+        Controls.Add(txtYearPublished)
+        Controls.Add(dgvBook)
+        Controls.Add(btnClear)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgvBook, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents dgvBook As DataGridView
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+
+End Class
